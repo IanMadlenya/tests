b9076c1eaf927dd2c39762fea37da046d10bf167
Jack Peterson <jack@tinybike.net>
2015-09-06 19:19:50 -0700

    Fixed path error

--- runtests.sh
+++ runtests.sh
@@ -11,45 +11,52 @@ GRAY='\033[1;30m'
 NC='\033[0m'
 
 HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+LOG="${HERE}/tests.log"
 
-declare -a repos=("augur-abi" "ethrpc" "keythereum" "augur.js" "augur-core")
+declare -a repos=("augur-abi" "ethrpc" "keythereum" "augur-core" "augur.js")
 
 echo -e "+${GRAY}==================${NC}+"
 echo -e "${GRAY}| \033[1;35maugur${NC} test suite ${GRAY}|${NC}"
 echo -e "+${GRAY}==================${NC}+\n"
 
+# delete log file
 if [ -f "${HERE}/tests.log" ]; then
-    rm tests.log >>$HERE/tests.log 2>&1
+    rm "${HERE}/tests.log" >>"${LOG}" 2>&1
 fi
 
 for repo in "${repos[@]}"
 do
+    fullpath="${HERE}/${repo}"
     url="https://github.com/AugurProject/${repo}"
     echo -e " - ${TEAL}${repo}${NC} ${GRAY}[${url}]${NC}"
 
-    if [ -d "${HERE}/${repo}" ]; then
-        rm -rf "${HERE}/${repo}" >>$HERE/tests.log 2>&1
+    # remove existing directory
+    if [ -d "${fullpath}" ]; then
+        rm -rf "${fullpath}" >>"${LOG}" 2>&1
     fi
 
-    git clone "${url}" >>$HERE/tests.log 2>&1
-    cd "${HERE}/${repo}"
+    # clone and install repo
+    git clone "${url}" "${fullpath}" >>"${LOG}" 2>&1
+    cd "${fullpath}"
     if [ "${repo}" == "augur-core" ]; then
-        virtualenv venv >>$HERE/tests.log 2>&1
-        source "${HERE}/${repo}/venv/bin/activate" >>$HERE/tests.log 2>&1
-        pip install -r requirements-load.txt >>$HERE/tests.log 2>&1
+        virtualenv venv >>"${LOG}" 2>&1
+        source "${fullpath}/venv/bin/activate" >>"${LOG}" 2>&1
+        pip install -r requirements-load.txt >>"${LOG}" 2>&1
     else
-        npm install >>$HERE/tests.log 2>&1
+        npm install >>"${LOG}" 2>&1
     fi
 
+    # run unit tests
     if [ "${repo}" == "augur.js" ]; then
         npm run testnet
     elif [ "${repo}" == "augur-core" ]; then
-        python "${HERE}/${repo}/tests/test_load_contracts.py"
-        deactivate >>$HERE/tests.log 2>&1
+        python "${fullpath}/tests/test_load_contracts.py"
+        deactivate >>"${LOG}" 2>&1
     else
         npm test -- -R progress
     fi
 
+    # clean up
     cd "${HERE}"
-    rm -rf "${HERE}/${repo}" >>$HERE/tests.log 2>&1
+    rm -rf "${fullpath}" >>LOG 2>&1
 done
